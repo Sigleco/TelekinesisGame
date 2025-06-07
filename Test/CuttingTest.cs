33125557a2cf1a20022dcf75eee1ce16c25fd0dd
Sigleco <pandriukha@mail.ru>
2025-06-07 15:59:24 +0700

    Fixed the tests. They now are working with any contact point.

--- Test/CuttingTest.cs
+++ Test/CuttingTest.cs
@@ -7,7 +7,7 @@ using UnityEngine.UIElements;
 
 public class EnumeratorTest : MonoBehaviour
 {
-    private Vector3 _contactPoint = new Vector3(0, -0.5f, 0);
+    private Vector3 _contactPoint = new Vector3(0, 0, 0.2f);
     private Vector3 _dirV = Vector3.right;
     private Vector3 _dirU = Vector3.up;
     private Mesh _mesh;
@@ -49,7 +49,6 @@ public class EnumeratorTest : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            //Divide();
             TriangleDivide();
             leftMesh = CreateNewMeshes(leftSides);
             GameObject obj = new GameObject { transform = {position = Vector3.right * 1f}};
@@ -264,228 +263,9 @@ public class EnumeratorTest : MonoBehaviour
         Vector3 normal = temp - Vector3.Dot(temp, cross) * cross;
         return normal.normalized;
     }
-    //////////////////////////////////////////////////////////////////////////////
-    /*
-    private void Divide()
-    {
-        List<int> unUsedVertices = Enumerable.Range(0, _mesh.vertices.Length).ToList();
-        while (unUsedVertices.Count > 0)
-        {
-            DivideSide(unUsedVertices);
-        }
-        
-        checkedVectors = checkedVectors.Distinct().ToList();
-        AddLastSide(checkedVectors, ref leftSides, ref rightSides);
-    }
-
-    private void DivideSide(List<int> unUsedVertices)
-    {
-        List<int> side = FindSide(unUsedVertices);
-        BuildNewSides(side);
-    }
-
-    private List<int> FindSide(List<int> unusedVertices)
-    {
-        List<int> sideVertices = new List<int>();
-        sideVertices.Add(unusedVertices[0]);
-        unusedVertices.Remove(sideVertices[0]);
-
-        CheckNextGenDots(ref sideVertices,  0,  1);
-        unusedVertices.RemoveAll(x => sideVertices.Contains(x));
-        return sideVertices;
-    }
-
-    private void CheckNextGenDots(ref List<int> sideVertices, int startIndex, int amountToCheck)
-    {
-        for (int j = startIndex; j < amountToCheck; j++)
-        {
-            for (int i = 0; i < _mesh.triangles.Length; i++)
-            {
-                if (sideVertices[j] == _mesh.triangles[i])
-                {
-                    List<int> triangle = GetTriangle(i);
-                    foreach (int vert in triangle)
-                    {
-                        sideVertices.Add(_mesh.triangles[vert]);
-                    }
-                }
-            }
-        }
-
-        int rawSize = sideVertices.Count;
-        sideVertices = sideVertices.Distinct().ToList();
-        if (rawSize != sideVertices.Count)
-        {
-            startIndex += amountToCheck;
-            int temp = sideVertices.Count - startIndex;
-            CheckNextGenDots( ref sideVertices,  startIndex, temp);
-        }
-    }
-
-    private void BuildNewSides(List<int> side)
-    {
-        Vector3 crossProduct = Vector3.Cross(_dirU, _dirV);
-        Vector3 newVertex1 = Vector3.zero, newVertex2 = Vector3.zero;
-
-        List<(int, float)> left = new List<(int, float)>();
-        List<(int, float)> right = new List<(int, float)>();
-        
-        for (int i = 0; i < side.Count; i++)
-        {
-            float temp = Vector3.Dot(crossProduct, _contactPoint + _mesh.vertices[side[i]]) / crossProduct.magnitude;
-            if (temp >= 0)
-            {
-                left.Add((side[i], temp));
-            }
-            else
-            {
-                right.Add((side[i], temp));
-            }
-        }
-        
-        (newVertex1, newVertex2) = FindNewVertices(left, right);
-        
-        List<Vector3> _right = right.ConvertAll(x => _mesh.vertices[x.Item1]);
-        List<Vector3> _left = left.ConvertAll(x => _mesh.vertices[x.Item1]);
-        
-        if (right.Count > 0 && left.Count > 0)
-        {
-            _right.InsertRange(0, new []{newVertex1, newVertex2});
-            int[] rightTriangles = CreateTriangles(_right, _mesh.normals[right[0].Item1]);
-            rightSides.Add(new Side(rightTriangles, _right.ToArray(), _mesh.normals[right[0].Item1], _mesh.tangents[right[0].Item1]));
-            
-            _left.InsertRange(0, new []{newVertex1, newVertex2});
-            int[] leftTriangles = CreateTriangles(_left, _mesh.normals[left[0].Item1]);
-            leftSides.Add(new Side(leftTriangles, _left.ToArray(), _mesh.normals[left[0].Item1], _mesh.tangents[left[0].Item1]));
-            
-            checkedVectors.Add(newVertex1);
-            checkedVectors.Add(newVertex2);
-        }
-        else if (left.Count > 0 && right.Count == 0)
-        {
-            int[] leftTriangles = CreateTriangles(_left, _mesh.normals[left[0].Item1]);
-            leftSides.Add(new Side(leftTriangles, _left.ToArray(), _mesh.normals[left[0].Item1], _mesh.tangents[left[0].Item1]));
-        }
-        else if (right.Count > 0 && left.Count == 0)
-        {
-            int[] rightTriangles = CreateTriangles(_right, _mesh.normals[right[0].Item1]);
-            rightSides.Add(new Side(rightTriangles, _right.ToArray(), _mesh.normals[right[0].Item1], _mesh.tangents[right[0].Item1]));
-        }
-    }
     
-    private (Vector3, Vector3) FindNewVertices(List<(int, float)> left, List<(int, float)> right)
-    {
-        Vector3 newVertex1 = Vector3.zero, newVertex2 = Vector3.zero;
-        
-        if (left.Count > 1 && right.Count > 1)
-        {
-            List<float> fl = new List<float>();
-            List<Vector3> points = new List<Vector3>();
-            
-            left.Sort((p1, p2) => p1.Item2.CompareTo(p2.Item2));
-            right.Sort((p1, p2) => -1 * p1.Item2.CompareTo(p2.Item2));
-
-            for (int i = 0; i < left.Count; i++)
-            {
-                for (int j = 0; j < right.Count; j++)
-                {
-                    points.Add(ComputeIntersectionPoint(_mesh.vertices[left[i].Item1], _mesh.vertices[right[j].Item1]));
-                }
-            }
-
-            Vector3 cross = Vector3.Cross(points[0], points[1]);
-            for (int i = 1; i < 4; i++)
-            {
-                fl.Add(Vector3.SignedAngle(points[0], points[i], cross));
-            }
-
-            if (fl[0] > 0 && fl[1] > 0 && fl[2] > 0)
-            {
-                newVertex1 = points[0];
-                newVertex2 = points[fl.IndexOf(fl.Max()) + 1];
-            }
-            else if (-fl[0] > 0 && -fl[1] > 0 && -fl[2] > 0)
-            {
-                newVertex1 = points[0];
-                newVertex2 = points[fl.IndexOf(fl.Min()) + 1];
-            }
-            else
-            {
-                newVertex1 = points[fl.IndexOf(fl.Max()) + 1];
-                newVertex2 = points[fl.IndexOf(fl.Min()) + 1];
-            }
-        }
-        else if (left.Count == 1)
-        {
-            right.Sort((p1, p2) => -1 * p1.Item2.CompareTo(p2.Item2));
-            newVertex1 = ComputeIntersectionPoint(_mesh.vertices[left[0].Item1], _mesh.vertices[right[0].Item1]);
-            newVertex2 = ComputeIntersectionPoint(_mesh.vertices[left[0].Item1], _mesh.vertices[right[1].Item1]);
-        }
-        else if(right.Count == 1)
-        {
-            left.Sort((p1, p2) => p1.Item2.CompareTo(p2.Item2));
-            newVertex1 = ComputeIntersectionPoint(_mesh.vertices[right[0].Item1], _mesh.vertices[left[0].Item1]);
-            newVertex2 = ComputeIntersectionPoint(_mesh.vertices[right[0].Item1], _mesh.vertices[left[1].Item1]);
-        }
-
-        return (newVertex1, newVertex2);
-    }
-
-    private int[] CreateTriangles(List<Vector3> vertices, Vector3 normal)
-    {
-        int[] triangles = new int[(vertices.Count - 2) * 3];
-        Vector3 mainLine = vertices[1] - vertices[0];
-        List<(Vector3, int, float)> maps = vertices.ConvertAll(x => (x, vertices.IndexOf(x), 0f));
-        for (int i = 2; i < vertices.Count; i++)
-        {
-            maps[i] = (maps[i].Item1, maps[i].Item2, Vector3.SignedAngle(mainLine, vertices[i] - vertices[0],  normal));
-        }
-        
-        maps.Sort(1, maps.Count - 1, Comparer<(Vector3, int, float)>.Create((p1, p2) => p1.Item3.CompareTo(p2.Item3)));
-
-        for (int i = 0; i < maps.Count - 2; i++)
-        {
-            triangles[3 * i] = maps[0].Item2;
-            triangles[3 * i + 1] = maps[i + 1].Item2;
-            triangles[3 * i + 2] = maps[i + 2].Item2;
-        }
-
-        return triangles;
-    }
-
     private void AddLastSide(List<Vector3> vertices, ref List<Side> sides, ref List<Side> oppositeSides)
     {
-        vertices = vertices.Distinct().ToList();
-        Vector3 nm = Vector3.Cross(vertices[0] - vertices[1], vertices[0] - vertices[2]);
-
-        Vector3[] sideVertices = sides[0].GetVertices();
-        float[] angles = new float[2];
-        
-        for (int i = 0; i < angles.Length; i++)
-        {
-            angles[i] = Vector3.Angle(nm, sideVertices[i] - vertices[0]);
-        }
-
-        if (angles[0] < 90 && angles[1] < 90)
-        {
-            nm *= -1;
-        }
-
-        int[] triangles = CreateTriangles(vertices, nm);
-        
-        sides.Add(new Side((int[])triangles.Clone(), vertices.ToArray(), nm, vertices[0] - vertices[1]));
-
-        for (int i = 0; i < triangles.Length;i +=3)
-        {
-            (triangles[i + 1], triangles[i + 2]) = (triangles[i + 2], triangles[i + 1]);
-        }
-
-        oppositeSides.Add(new Side(triangles, vertices.ToArray(), -1 * nm, vertices[0] - vertices[1]));
-    }
-    */
-    private void AddLastSide(List<Vector3> vertices, ref List<Side> sides, ref List<Side> oppositeSides)
-    {
-        vertices = vertices.Distinct().ToList();
         Vector3 nm = Vector3.zero;
         for (int i = 2; i < vertices.Count; i++)
         {
@@ -715,25 +495,31 @@ public class EnumeratorTest : MonoBehaviour
     private void CoplanarityTest(List<Vector3> vertices)
     {
         int counter = 0;
-        Vector3 normal = Vector3.Cross(vertices[0], vertices[1]);
-
-        for (int i = 2; i < vertices.Count; i++)
+        Vector3 normal = Vector3.forward;
+        
+        for (int i = 1; i < vertices.Count; i++)
         {
-            if (Mathf.Abs(Vector3.Dot(normal, vertices[i])) <=  0.01f)
+            float temp = Vector3.Dot(normal, vertices[0] - vertices[i]);
+            if (temp is <= 0.01f and >= -0.01f)
             {
                 counter++;
             }
         }
-        
-        Debug.Log("Coplanar vertices" + (counter + 2) + "/" + vertices.Count);
+
+        Debug.Log("Coplanar vertices" + (counter + 1) + "/" + vertices.Count);
     }
 
     private void FullRotationTest(List<Vector3> vertices)
     {
         float angle = 0;
-        Vector3 normal = Vector3.Cross(vertices[0], vertices[1]);
+        Vector3 normal = Vector3.forward;
         List<(Vector3, float)> vertexAngelMap = new List<(Vector3, float)>();
 
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            vertices[i] -=  _contactPoint;
+        }
+        
         for (int i = 0; i < vertices.Count; i++)
         {
             vertexAngelMap.Add((vertices[i], Vector3.SignedAngle(vertices[0], vertices[i], normal)));
@@ -744,6 +530,7 @@ public class EnumeratorTest : MonoBehaviour
         for (int i = 0; i < vertexAngelMap.Count-1; i++)
         {
             angle += Vector3.SignedAngle(vertexAngelMap[i].Item1, vertexAngelMap[i+1].Item1, normal);
+            Debug.Log(angle);
         }
         
         angle += Vector3.SignedAngle(vertexAngelMap[^1].Item1, vertexAngelMap[0].Item1, normal);
@@ -766,6 +553,8 @@ public class Vector3Comparer : IEqualityComparer<Vector3>
         return Vector3.SqrMagnitude(v1 - v2) < epsilon * epsilon;
     }
 
+    
+    //Auxiliary classes
     public int GetHashCode(Vector3 v)
     {
 
